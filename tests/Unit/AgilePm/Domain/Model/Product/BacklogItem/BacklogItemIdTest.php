f7f37791e13b41a2044fd98a2ef5b213666c52d6
Michael Chekin <mchekin@gmail.com>
2019-09-13 18:21:48 +0200

    Adding BacklogItemId.

---
+++ tests/Unit/AgilePm/Domain/Model/Product/BacklogItem/BacklogItemIdTest.php
@@ -0,0 +1,20 @@
+<?php declare(strict_types=1);
+
+
+namespace Tests\Unit\AgilePm\Domain\Model\Tenant;
+
+use App\AgilePm\Domain\Model\Product\BacklogItem\BacklogItemId;
+use App\AgilePm\Domain\Model\Product\ProductId;
+use PHPUnit\Framework\TestCase;
+
+class BacklogItemIdTest extends TestCase
+{
+    public function testSetId()
+    {
+        $anId = '062be292-2718-44a8-aae6-7612cebaa9b9';
+
+        $productId = new BacklogItemId($anId);
+
+        $this->assertSame($anId, $productId->id());
+    }
+}
