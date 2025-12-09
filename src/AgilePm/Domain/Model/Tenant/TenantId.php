2240c2faa8f3efb331f2c93c79d59694ad7b2730
Michael Chekin <mchekin@gmail.com>
2025-12-09 19:29:36 +0100

    Complete TenantId implementation

--- src/AgilePm/Domain/Model/Tenant/TenantId.php
+++ src/AgilePm/Domain/Model/Tenant/TenantId.php
@@ -1,14 +1,40 @@
 <?php declare(strict_types=1);
 
-
 namespace App\AgilePm\Domain\Model\Tenant;
 
-use App\Common\Domain\Model\AbstractId;
+use Webmozart\Assert\Assert;
 
-class TenantId extends AbstractId
+final readonly class TenantId
 {
-    public function __construct(string $anId)
+    public function __construct(
+        private string $id
+    ) {
+        $this->validateId($id);
+    }
+
+    public static function fromTenantId(TenantId $tenantId): self
     {
-        parent::__construct($anId);
+        return new self($tenantId->id());
+    }
+
+    public function id(): string
+    {
+        return $this->id;
+    }
+
+    public function equals(TenantId $other): bool
+    {
+        return $this->id === $other->id;
+    }
+
+    public function __toString(): string
+    {
+        return "TenantId [id={$this->id}]";
+    }
+
+    private function validateId(string $id): void
+    {
+        Assert::stringNotEmpty($id, 'The tenant identity is required.');
+        Assert::maxLength($id, 36, 'The tenant identity must be 36 characters or less.');
     }
 }
