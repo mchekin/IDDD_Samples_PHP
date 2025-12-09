eb2f487e17f6e86e429738a35839f8088ba4130b
Michael Chekin <mchekin@gmail.com>
2025-12-09 20:23:29 +0100

    Complete ProductId implementation

--- src/AgilePm/Domain/Model/Product/ProductId.php
+++ src/AgilePm/Domain/Model/Product/ProductId.php
@@ -1,15 +1,49 @@
-<?php declare(strict_types=1);
+<?php
 
+declare(strict_types=1);
 
 namespace App\AgilePm\Domain\Model\Product;
 
+use App\AgilePm\Domain\Model\ValueObject;
 
-use App\Common\Domain\Model\AbstractId;
-
-class ProductId extends AbstractId
+final class ProductId extends ValueObject
 {
+    private string $id;
+
     public function __construct(string $anId)
     {
-        parent::__construct($anId);
+        parent::__construct();
+        $this->setId($anId);
+    }
+
+    public static function fromProductId(ProductId $productId): self
+    {
+        return new self($productId->id());
+    }
+
+    public function id(): string
+    {
+        return $this->id;
+    }
+
+    public function equals(ProductId $other): bool
+    {
+        return $this->id === $other->id;
+    }
+
+    public function hashCode(): int
+    {
+        return (57853 * 31) + crc32($this->id);
+    }
+
+    public function __toString(): string
+    {
+        return "ProductId [id={$this->id}]";
+    }
+
+    private function setId(string $id): void
+    {
+        $this->assertArgumentNotEmpty($id, 'The id must be provided.');
+        $this->assertArgumentLength($id, 36, 'The id must be 36 characters or less.');
     }
 }
