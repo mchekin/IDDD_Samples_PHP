ee16e40da9915cdbc030ed33df59176a826a5809
Michael Chekin <mchekin@gmail.com>
2025-12-09 20:42:59 +0100

    Complete ProductOwnerId implementation

--- src/AgilePm/Domain/Model/Team/ProductOwnerId.php
+++ src/AgilePm/Domain/Model/Team/ProductOwnerId.php
@@ -1,33 +1,25 @@
 <?php declare(strict_types=1);
 
-
 namespace App\AgilePm\Domain\Model\Team;
 
-
+use App\AgilePm\Domain\Model\ValueObject;
 use App\AgilePm\Domain\Model\Tenant\TenantId;
-use Webmozart\Assert\Assert;
 
-class ProductOwnerId
+final class ProductOwnerId extends ValueObject
 {
-    /**
-     * @var TenantId
-     */
-    private $aTenantId;
-    /**
-     * @var string
-     */
-    private $id;
+    private string $id;
+    private TenantId $tenantId;
 
     public function __construct(TenantId $aTenantId, string $anId)
     {
-        $this->aTenantId = $aTenantId;
-
+        parent::__construct();
         $this->setId($anId);
+        $this->setTenantId($aTenantId);
     }
 
-    public function tenantId(): TenantId
+    public static function fromProductOwnerId(ProductOwnerId $productOwnerId): self
     {
-        return $this->aTenantId;
+        return new self($productOwnerId->tenantId(), $productOwnerId->id());
     }
 
     public function id(): string
@@ -35,11 +27,36 @@ class ProductOwnerId
         return $this->id;
     }
 
+    public function tenantId(): TenantId
+    {
+        return $this->tenantId;
+    }
+
+    public function equals(ProductOwnerId $other): bool
+    {
+        return $this->tenantId->equals($other->tenantId) && $this->id === $other->id;
+    }
+
+    public function hashCode(): int
+    {
+        return (43685 * 83) + crc32($this->id);
+    }
+
+    public function __toString(): string
+    {
+        return "ProductOwnerId [tenantId={$this->tenantId}, id={$this->id}]";
+    }
+
     private function setId(string $anId): void
     {
-        Assert::stringNotEmpty($anId, 'The basic identity is required.');
-
+        $this->assertArgumentNotEmpty($anId, 'The id must be provided.');
+        $this->assertArgumentLength($anId, 36, 'The id must be 36 characters or less.');
         $this->id = $anId;
     }
 
+    private function setTenantId(TenantId $aTenantId): void
+    {
+        $this->assertArgumentNotNull($aTenantId, 'The tenantId must be provided.');
+        $this->tenantId = $aTenantId;
+    }
 }
