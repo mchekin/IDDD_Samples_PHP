06a17d8913bc7c292d6d683c19296169884fb9df
Michael Chekin <mchekin@gmail.com>
2025-12-09 20:21:34 +0100

    Add ValueObject

--- src/AgilePm/Domain/Model/ValueObject.php
+++ src/AgilePm/Domain/Model/ValueObject.php
@@ -1,10 +1,13 @@
 <?php declare(strict_types=1);
 
-
 namespace App\AgilePm\Domain\Model;
 
+use App\Common\AssertionConcern;
 
-abstract class ValueObject
+abstract class ValueObject extends AssertionConcern
 {
-
+    public function __construct()
+    {
+        parent::__construct();
+    }
 }
