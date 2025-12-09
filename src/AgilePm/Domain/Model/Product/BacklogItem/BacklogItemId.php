54d71df0bf34d5e454fd27ff96edd5553f95f873
Michael Chekin <mchekin@gmail.com>
2025-12-09 20:32:25 +0100

    Complete BacklogItemId implementation

--- src/AgilePm/Domain/Model/Product/BacklogItem/BacklogItemId.php
+++ src/AgilePm/Domain/Model/Product/BacklogItem/BacklogItemId.php
@@ -1,15 +1,47 @@
 <?php declare(strict_types=1);
 
-
 namespace App\AgilePm\Domain\Model\Product\BacklogItem;
 
+use App\AgilePm\Domain\Model\ValueObject;
 
-use App\Common\Domain\Model\AbstractId;
-
-class BacklogItemId extends AbstractId
+final class BacklogItemId extends ValueObject
 {
+    private string $id;
+
     public function __construct(string $anId)
     {
-        parent::__construct($anId);
+        parent::__construct();
+        $this->setId($anId);
+    }
+
+    public static function fromBacklogItemId(BacklogItemId $backlogItemId): self
+    {
+        return new self($backlogItemId->id());
+    }
+
+    public function id(): string
+    {
+        return $this->id;
+    }
+
+    public function equals(BacklogItemId $other): bool
+    {
+        return $this->id === $other->id;
+    }
+
+    public function hashCode(): int
+    {
+        return (57853 * 31) + crc32($this->id);
+    }
+
+    public function __toString(): string
+    {
+        return "BacklogItemId [id={$this->id}]";
+    }
+
+    private function setId(string $id): void
+    {
+        $this->assertArgumentNotEmpty($id, 'The id must be provided.');
+        $this->assertArgumentLength($id, 36, 'The id must be 36 characters or less.');
     }
 }
